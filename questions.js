7378cfdf8207f808f950a8f53d5577c34573d5ee
BxrsukDsU <163361332+BxrsukDsU@users.noreply.github.com>
2024-06-10 20:47:07 -0300

    upload dos arquivos

---
+++ questions.js
@@ -0,0 +1,81 @@
+export default [
+    {
+      question: "Um fazendeiro resolveu investir em uma colheitadeira para facilitar o serviço na plantação. Sabendo que o valor pago foi de R$ 300.000 no ano da compra, é bastante comum que máquinas desse porte percam o seu valor V ao decorrer dos anos t. Supondo que a taxa de depreciação de uma máquina desse porte é de R$ 22.000 por ano, devido ao seu constante uso, podemos afirmar que o valor da colheitadeira, ao final de 7 anos, será de:",
+      answers: [
+        { option: "A) R$ 154.000", correct: false },
+        { option: "B) R$ 246.000", correct: false },
+        { option: "C) R$ 146.000", correct: true },
+        { option: "D) R$ 174.000", correct: false },
+        { option: "E) R$ 210.000", correct: false },
+      ],
+    },
+    {
+      question: "O uso de aplicativos para realizar viagens é cada vez mais comum no cotidiano. Supõe-se que, para calcular o valor da viagem em um aplicativo, há um valor fixo mais um total de R$ 1,40 por quilômetros rodado. Sabendo que um cliente pago ao final da viagem, a quantidade de quilômetros rodados foi de 8 km, então o valor fixo da viagem foi de:",
+      answers: [
+        { option: "A) R$ 2", correct: false },
+        { option: "B) R$ 2,50", correct: false },
+        { option: "C) R$ 3,60", correct: false },
+        { option: "D) R$ 4,40", correct: true },
+        { option: "E) R$ 5", correct: false },
+      ],
+    },
+    {
+      question: "Seu Carlos está indeciso quanto a um plano de saúde que precisa escolher. O plano 1 custa R$310,00 por mês, mais R$16,00 por consulta. Já o plano 2 custa R$190,00 por mês, mais R$32,00 por consulta. Seu Carlos faz, em média, 8 consultas por mês. Qual dos dois planos é mais vantajoso para ele e qual a diferença final de gastos mensais com plano de saúde?",
+      answers: [
+        { option: "A) Plano 2, com diferença de 100 reais", correct: false },
+        { option: "B) Plano 2, com diferença de 50 reais", correct: false },
+        { option: "C) Plano 1, com diferença de 8 reais", correct: true },
+        { option: "D) Plano 1, com diferença de 100 reais", correct: false },
+        { option: "E) Plano 1, com diferença de 50 reais", correct: false },
+      ],
+    },
+    {
+      question: "Dada as funções de lei de formação f(x) = 2x + 5 e g(x) = -3x + 1, podemos afirmar que o valor de f (g(1)) é igual a:",
+      answers: [
+        { option: "A) 0", correct: false },
+        { option: "B) 1", correct: false },
+        { option: "C) 2", correct: false },
+        { option: "D) 3", correct: true },
+        { option: "E) 4", correct: false },
+      ],
+    },
+    {
+      question: "Dada a função f(x) = 2x + 1 e g(x) = x – 1, julgue as sentenças a seguir: " + "I → (f o g)(x) = (g o f)(x) " + " II → (g o f)(2) = 4" + " III → (f o g)(1) = 1 " + "As afirmativas são, respectivamente:",
+      answers: [
+        { option: "A) V, V e F", correct: false },
+        { option: "B) F, V e V", correct: true },
+        { option: "C) V, F e V", correct: false },
+        { option: "D) F, F e V", correct: false },
+        { option: "E) F, V e F", correct: false },
+      ],
+    },
+    {
+      question: "Se f(x) = x5 e g(x) = x – 1, a função composta f[g(x)] será igual a:",
+      answers: [
+        { option: "a) x5 + x – 1", correct: false},
+        { option: "b) x6 – x5", correct: false},
+        { option: "c) x6 – 5x5 + 10x4 – 10x3 + 5x2 – 5x + 1", correct: false},
+        { option: "d) x5 – 5x4 + 10x3 – 10x2 + 5x – 1", correct: true},
+        { option: "e) x5 – 5x4 – 10x3 – 10x2 – 5x – 1", correct: false},
+      ],
+    },
+    {
+      question: "Dadas as funções reais f(x) = 2x – 6 e g(x) = ax + b, se f[g(x)] = 12x + 8, o valor de a + b é:",
+      answers: [
+        { option: "a) 10", correct: false},
+        { option: "b) 13", correct: true},
+        { option: "c) 12", correct: false},
+        { option: "d) 20", correct: false},
+      ],
+    },
+    {
+      question: "Na função afim dada por f(x) = 4x – (8 + 2x), se utilizarmos o elemento do domínio 9, teremos como imagem:",
+      answers: [
+        { option: "a) 10", correct: true},
+        { option: "b) 12", correct: false},
+        { option: "c) 14", correct: false},
+        { option: "d) 16", correct: false},
+        { option: "e) 18", correct: false},
+      ],
+    },
+  ];
